573fe4e76dd1357418ff268e6dc5379f3f9729ad
SimbaFs <simba.fs@gmail.com>
2023-07-30 01:29:48 +0800

    finish outline except saving API data and css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,102 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { useReducer, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getStaticProps() {
+  let data = await fetch('http://localhost:3000/session.json').then(res => res.json())
 
-export default function Home() {
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default function Home({ data }) {
+  let rooms = Array.from(new Set(data.sessions.map(i => i.room)))
+  let [day, setDay] = useState(29)
+  let [room, setRoom] = useReducer((oldRoom, newRoom) => {
+    if (rooms.includes(newRoom)) {
+      return newRoom
+    }
+    return oldRoom
+  }, 'AU')
+
+  // update = {day: 29, room: 'AU', id: 'V8F9VH', attendance: 10}
+  let [attendance, updateAttendance] = useReducer((curr, update) => {
+    let r = {
+      29: curr[29],
+      30: curr[30],
+    }
+    r[update.day][update.room][update.id] = update.attendance
+
+    return r
+  }, {
+    29: Object.fromEntries(rooms.map(i => [i, {}])),
+    30: Object.fromEntries(rooms.map(i => [i, {}])),
+  })
+
+  let sessions = groupBy(data.sessions.filter(item => (new Date(item.start)).getDate() == day), 'room')[room]
+  // sort by start time
+  sessions.sort((a, b) => {
+    let timeA = new Date(a.start)
+    let timeB = new Date(b.start)
+    return timeA - timeB
+  })
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>製播組統計議程人數統計</title>
+        <link href="https://coscup.org/2023/favicon.svg" rel="icon" type="image/svg+xml" />
+      </Head>
+      <h1>製播組統計議程人數統計</h1>
+      <select value={day} onChange={e => setDay(e.target.value)}>
+        <option value={29}>29</option>
+        <option value={30}>30</option>
+      </select>
+      <select value={room} onChange={e => setRoom(e.target.value)}>
+        {rooms.map(room => <option key={room} value={room}>{room}</option>)}
+      </select>
+      {sessions.map(s => <Session
+        key={s.id}
+        session={s}
+        attendance={attendance[day][s.room][s.id]}
+        setAttendance={n => updateAttendance({
+          day: day,
+          room: s.room,
+          id: s.id,
+          attendance: n,
+        })}
+      />)}
+    </>
   )
+}
+
+function groupBy(arr, key) {
+  let result = {}
+  arr.forEach(item => {
+    let value = item[key]
+    if (!result[value]) {
+      result[value] = []
+    }
+    result[value].push(item)
+  })
+  return result
+}
+
+function getFormatedDate(dateStr) {
+  let time = new Date(dateStr)
+  let to2 = n => n < 10 ? `0${n}` : `${n}`
+
+  return `${to2(time.getHours())}:${to2(time.getMinutes())}`
+}
+
+function Session({ session, attendance, setAttendance }) {
+  return <>
+    <div className='w-screen space-x-2'>
+      <span>{getFormatedDate(session.start)}-{getFormatedDate(session.end)}</span>
+      <span>{session.zh.title}</span>
+      <input type='number' value={attendance} onChange={e => setAttendance(e.target.value)} />
+    </div>
+  </>
 }
