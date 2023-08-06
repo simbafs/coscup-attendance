0fa979db0d694076f448c9e7b1959069794d0ae6
SimbaFs <simba.fs@gmail.com>
2023-08-06 23:58:28 +0800

    update footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,12 +96,17 @@ export default function Home() {
 					)}
 				</div>
 				<div className="grow" />
-				<footer className="flex justify-center pb-8 pt-4 w-full bg-gray-100">
+				<footer className="flex justify-center items-center flex-col pb-8 pt-4 w-full bg-gray-100">
 					<p>Repo at <a
 						href="https://github.com/simbafs/coscup-attendance"
 						className='text-blue-500 underline hover:underline-offset-0'
 						target="_blank"
 					>simbafs/coscup-attendance</a></p>
+					<p>Made by <a
+						href="https://github.com/simbafs"
+						className='text-blue-500 underline hover:underline-offset-0'
+						target="_blank"
+					>SimbaFs</a> with 🐈</p>
 				</footer>
 			</div>
 		</>
